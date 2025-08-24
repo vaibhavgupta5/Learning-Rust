789f6a1e711b46beda2f82c74f0453bb40a01a4a
vaibhavgupta5 <vaibhavgupta.v890@gmail.com>
2025-08-25 03:01:45 +0530

    Add new dependencies, update main logic, and enhance documentation
    - Added new packages to Cargo.lock for improved functionality.
    - Updated Cargo.toml to include necessary dependencies.
    - Created a new text file for content storage.
    - Refactored main.rs to implement shape area calculations and file reading.
    - Enhanced rust-book.md with additional examples and explanations.

--- rust-book.md
+++ rust-book.md
@@ -1,22 +1,72 @@
 # Here y is a int datatype (as x+1 do not have ;)
 
-fn main() {
-    let y = {
-        let x = 3;
-        x + 1
-    };
+    fn main() {
+        let y = {
+            let x = 3;
+            x + 1
+        };
 
-    println!("The value of y is: {y}");
-}
+        println!("The value of y is: {y}");
+    }
 
 # Here y is a function (as x+1 has ;)
 
-fn main() {
-    let y = {
-        let x = 3;
-        x + 1;
+    fn main() {
+        let y = {
+            let x = 3;
+            x + 1;
+        };
+
+        println!("The value of y is: {y}");
+        //Error
+    }
+
+
+##### Clone: .clone()
+
+# enms
+
+    enum Shape {
+        Circle(f64),
+        Square(f64),
+        Rectangle(f64, f64),
+    }
+
+     let ans: f64 = match shape {
+        Shape::Circle(radius) => 3.14 * radius * radius,
+
+        Shape::Rectangle(width, height) => width * height,
+
+        Shape::Square(side) => side * side,
     };
 
-    println!("The value of y is: {y}");
-    //Error
-}
+# stuct
+
+    struct Point<T> {
+        x: T,
+        y: T,
+    }
+
+ T can be of any type.
+
+ # try-catch
+
+    match file{
+        Ok (some) =>{
+            func
+        }
+        Err (some) =>{
+            func
+        }
+    }
+
+# Optional
+
+    match str {
+        Some (s) => {
+            func
+        }
+        None => {
+            func
+        }
+    }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,91 @@
+use std::fs;
+
 //This file might heavily change
-
-fn main() {
-    let x = plus_one(5);
-
-    println!("The value of x is: {x}");
+enum Shape {
+    Circle(f64),
+    Square(f64),
+    Rectangle(f64, f64),
 }
 
-fn plus_one(x: i32) -> i32 {
-   return  x + 1;
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+// enum Try <A, B> {
+//     Ok(A),
+//     Err(B),
+// }
+
+fn main() {
+    println!(
+        "The area of the circle is: {}",
+        calc_area(Shape::Circle(5.0))
+    );
+
+    println!(
+        "The area of the square is: {}",
+        calc_area(Shape::Square(5.0))
+    );
+
+    println!(
+        "The area of the rectangle is: {}",
+        calc_area(Shape::Rectangle(5.0, 10.0))
+    );
+
+    let point: Point<i32> = Point { x: 5, y: 10 };
+
+    println!("The coordinates of the point are: ({}, {})", point.x, point.y);
+
+    let file = fs::read_to_string("./public/text.txt");
+
+    match file {
+        Ok(content) => {
+            println!("File content: {}", content);
+        }
+        Err(error) => {
+            println!("Error reading file: {}", error);
+        }
+    }
+
+let random_u8: u8 = rand::random();
+
+
+println!("char: '{}'", random_u8 as i32);
+
+let stt = String::from("Vaibhav");
+
+doSomething(&stt);
+
+let stt2 = &stt;
+
+println!("String after function call: {}", stt2);
+
+// value ownership is now doSomething
+println!("String after function call: {}", stt);
+
+let mut vec = Vec::new();
+
+vec.push(1);
+vec.push(2);
+vec.push(3);
+
+println!("Vector: {:?}", vec);
+
+}
+
+fn doSomething (s : &String){
+    println!("{}", s);
+}
+
+fn calc_area(shape: Shape) -> f64 {
+    let ans: f64 = match shape {
+        Shape::Circle(radius) => 3.14 * radius * radius,
+
+        Shape::Rectangle(width, height) => width * height,
+
+        Shape::Square(side) => side * side,
+    };
+
+    return ans;
 }
